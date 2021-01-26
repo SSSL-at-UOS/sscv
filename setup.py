1ba434e72f86960cac6e675a6bab2ca33739256f
Byunghyun Kim <39578903+StringBottle@users.noreply.github.com>
2021-01-26 14:30:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         # 패키지에 대한 태그
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
