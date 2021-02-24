2a4c53a6bde71ca75ea7600c1e4a0de92e0ce3c0
stringbottle <stringbottle.mail@gmail.com>
2021-02-24 18:01:25 +0900

    Upload to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sscv",
-    version="0.1.0",
+    version="0.1.12",
     license='MIT',
     author="Byunghyun Kim",
     author_email="kimbh.mail@gmail.com",
